4cd2aa4f31c82e7eb8d0a076c316eb1f93763028
azzamdhx <abdullahbasohalim@gmail.com>
2026-01-28 15:24:33 +0700

    Fix Redis connection timeout

--- internal/database/redis.go
+++ internal/database/redis.go
@@ -2,22 +2,37 @@ package database
 
 import (
 	"context"
+	"log"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
 
 func NewRedis(redisURL string) (*redis.Client, error) {
+	if redisURL == "" {
+		log.Println("REDIS_URL not set, skipping Redis connection")
+		return nil, nil
+	}
+
 	opt, err := redis.ParseURL(redisURL)
 	if err != nil {
 		return nil, err
 	}
 
+	opt.DialTimeout = 10 * time.Second
+	opt.ReadTimeout = 5 * time.Second
+	opt.WriteTimeout = 5 * time.Second
+
 	client := redis.NewClient(opt)
 
-	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
 	if err := client.Ping(ctx).Err(); err != nil {
+		log.Printf("Failed to connect to Redis: %v", err)
 		return nil, err
 	}
 
+	log.Println("Connected to Redis successfully")
 	return client, nil
 }
