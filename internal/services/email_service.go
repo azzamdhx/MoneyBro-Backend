16557f1155120d939ef2302fc42f045e84dccdbf
azzamdhx <abdullahbasohalim@gmail.com>
2026-02-06 15:28:19 +0700

    New Email Domain

--- internal/services/email_service.go
+++ internal/services/email_service.go
@@ -20,7 +20,7 @@ type EmailService struct {
 func NewEmailService(apiKey, templatesDir string) *EmailService {
 	return &EmailService{
 		client:       resend.NewClient(apiKey),
-		fromEmail:    "MoneyBro <noreply@moneybro-noreply.idramaflix.com>",
+		fromEmail:    "MoneyBro <noreply@moneybro.my.id>",
 		templatesDir: templatesDir,
 	}
 }
